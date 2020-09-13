4e9dfb6064af5c625b5db8082c00106446112154
Greg Brimble <developer@gregbrimble.com>
2020-09-13 11:11:39 +0100

    Add debug env and move prebuild step

--- packages/e2e/jest-playwright.config.js
+++ packages/e2e/jest-playwright.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   serverOptions: {
-    command: "cd ../../ && npm run prebuild && npx wrangler dev --port 8787",
+    command:
+      "cd ../.. && export RUST_LOG=debug && npx wrangler dev --port 8787",
     port: 8787,
     launchTimeout: 30000,
   },
