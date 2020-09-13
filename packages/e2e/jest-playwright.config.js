a31d914fe751f92ee217319ec82617ef910acfb5
Greg Brimble <developer@gregbrimble.com>
2020-09-13 10:58:30 +0100

    Add client prebuild step

--- packages/e2e/jest-playwright.config.js
+++ packages/e2e/jest-playwright.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   serverOptions: {
-    command: "cd ../../ && npx wrangler dev --port 8787",
+    command: "cd ../../ && npm run prebuild && npx wrangler dev --port 8787",
     port: 8787,
     launchTimeout: 30000,
   },
